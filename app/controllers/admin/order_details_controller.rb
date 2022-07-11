cb54c35f7e659a659307594727fad65df16010f1
EC2 Default User <ec2-user@ip-172-31-94-249.ec2.internal>
2022-07-11 09:28:12 +0000

    [Finish]

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,8 +2,15 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order_details = OrderDetail.find(params[:id])
+    @order = @order_details.order
     @order_details.update(order_detail_params)
-    redirect_to admin_order_path
+    if @order_details.making_status == "work"
+       @order.update(status: "work")
+    end
+    if @order.order_details.count == @order.order_details.where(making_status: 3).count
+       @order.update(status: "preparation")
+    end
+    redirect_to admin_order_path(@order)
   end
 
   private
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,6 +9,12 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    if @order.status == "confirmation"
+       @order.order_details.each do |order_detail|
+         order_detail.update(making_status: "wait")
+       end
+    end
+
     redirect_to admin_order_path
   end
 
